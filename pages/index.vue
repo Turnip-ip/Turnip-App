099f5ce7b28d2ec910a80288e9b9dd000a7d3d75
Hugo Martel <hugo.martel@ens-lyon.fr>
2024-10-09 00:01:22 +0200

    Linking is done with the WebAssembly Rust library via npm

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,40 @@
       </ResizablePanel>
       <ResizableHandle with-handle />
       <ResizablePanel>
-        <GraphView class="m-4 w-44 h-44" :dot="dot"/>
+        <GraphView class="m-4 w-72 h-82" :dot="dot"/>
       </ResizablePanel>
     </ResizablePanelGroup>
 </template>
 
 <script setup lang="ts">
+import init, { tm_string_to_dot } from "tm_parser/tm_parser.js?init";
 
-const dotArea = ref<string>("digraph {a -> c}")
-const dot = computed(() => dotArea.value)
+onMounted(() => {
+  init().then(() => {
+    console.log("Init finished")
+    inited.value = true
+  });
+});
+
+let inited = ref(false);
+
+const dotArea = ref<string>(`START
+| b -> (b,R), START
+| _ -> (_,L), q
+
+q
+| 1 -> (0,L), q
+| 0 -> (1,L), END
+`)
+const dot = computed(() => {
+  //await init();
+  //console.log("Computing", inited.value, dotArea.value)
+  if (inited.value) {
+    const a = tm_string_to_dot(dotArea.value, 'TEST');
+    console.log(a);
+    return a
+  } else {
+    return ""
+  }
+});
 </script>
