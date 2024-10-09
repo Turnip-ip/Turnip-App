d2bcdd8cc9a334eaf0530c7900ed69fb742b9265
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-09 14:14:49 +0200

    Add linter and formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
-// Your custom configs here
+import eslintPluginVue from "eslint-plugin-vue";
+import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
+
+export default withNuxt(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...eslintPluginVue.configs["flat/recommended"],
+  {
+    ignores: ["components/ui/"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        projectService: true,
+      },
+    },
+  },
+  {
+    files: ["layouts/*.vue", "pages/**/*.vue"],
+    rules: { "vue/multi-word-component-names": "off" },
+  },
+);
