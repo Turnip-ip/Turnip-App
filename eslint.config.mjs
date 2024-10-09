f321db5441cc3e98aa9c33ff598e9d1be59c122a
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-09 14:32:10 +0200

    Improve eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
+// @ts-expect-error we don't need type for this module
 import eslintPluginVue from "eslint-plugin-vue";
 import tseslint from "typescript-eslint";
+// @ts-expect-error we don't need type for this module
 import eslint from "@eslint/js";
 
 export default withNuxt(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   eslint.configs.recommended,
+  // @ts-expect-error The config return a lint error
   ...tseslint.configs.strictTypeChecked,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
   ...eslintPluginVue.configs["flat/recommended"],
   {
     ignores: ["components/ui/"],
@@ -18,6 +23,11 @@ export default withNuxt(
         parser: "@typescript-eslint/parser",
         projectService: true,
       },
+    },
+  },
+  {
+    rules: {
+      "vue/max-attributes-per-line": "off",
     },
   },
   {
