b9b508a0fcb5abcbda9acb0532c2d8c9cd5509ab
Ines BLANCHE <ines.blanche@ens-lyon.fr>
2024-10-11 17:17:35 +0200

    4 blocks separes

---
+++ components/ui/leftPanel/leftHalfPanel.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :style="{ backgroundColor: bgColor, padding: '16px', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }">
+        <h2> Code Editor Space</h2>
+        <slot>
+      </slot>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const props = defineProps({
+    bgColor: {
+      type: String,
+      default: 'pink'    
+    },
+    height: {
+      type: String,
+      default: '1000px'    
+    }
+  })
+  </script>
+  
+  <style scoped>
+  /* You can add additional styles here if necessary */
+  </style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,46 @@
 <template>
     <ResizablePanelGroup direction="horizontal">
       <ResizablePanel>
-        <TextEditor v-model="dotArea" class="m-4" height="500px"/>
+        <LeftHalfPanel > 
+          <TextEditor v-model="dotArea" class="m-4" height="1000px"/>
+        </LeftHalfPanel>
+      
       </ResizablePanel>
       <ResizableHandle with-handle />
-      <ResizablePanel>
-        <GraphView class="m-4 w-44 h-44" :dot="dot"/>
+      <ResizablePanel height = '100%'>
+        <RightHalfPanel> 
+          <h2> Right Half Panel space</h2>
+          <div style="height: 33.33%; overflow: hidden;">
+            <GraphPanel>
+            <div style="height: 33.33%; overflow: hidden;">
+            <GraphView class="m-4 w-44 h-44" :dot="dot"/>
+          </div>
+            </GraphPanel>
+          
+
+          </div>
+         
+          
+          <div style="height: 33.33%; overflow: hidden;">
+            <RubanPanel/>
+          </div>
+          <div style="height: 33.33%; overflow: hidden;">
+            <OutputPanel/>
+          </div>
+         
+        </RightHalfPanel>
+        
       </ResizablePanel>
     </ResizablePanelGroup>
 </template>
 
 <script setup lang="ts">
+import LeftHalfPanel from '../components/ui/leftPanel/leftHalfPanel.vue';
+import GraphPanel from '../components/ui/rightPanel/GraphPanel.vue';
+import OutputPanel from '../components/ui/rightPanel/OutputPanel.vue';
+import RightHalfPanel from '../components/ui/rightPanel/RightHalfPanel.vue';
+import RubanPanel from '../components/ui/rightPanel/RubanPanel.vue';
+
 
 const dotArea = ref<string>("digraph {a -> c}")
 const dot = computed(() => dotArea.value)
