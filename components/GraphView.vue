649e42ed449464b719d7c19e5af1acff8814d8fd
Armand Didierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-15 11:47:39 +0200

    Add linter and formatter (#2)
    * Add linter and formatter
    
    * VS Code configuration
    
    * Github Workflow
    
    * Improve eslint and prettier config
    
    * Format project
    
    * Sort TailwindCSS classes
    
    * VS Code config for TailwindCSS
    
    * Format
    
    * Fix workflow lint command
    
    * Documentation
    
    * Rename lint workflow

--- components/GraphView.vue
+++ components/GraphView.vue
@@ -1,46 +1,58 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
-import type { Graphviz } from 'd3-graphviz';
-import { graphviz } from 'd3-graphviz';
-import * as d3 from 'd3';
+import { onMounted, ref, watch } from "vue";
+import type { Graphviz } from "d3-graphviz";
+import { graphviz } from "d3-graphviz";
+import * as d3 from "d3";
+import type { BaseType } from "d3-selection";
 
 interface Props {
-  dot: string
+  dot: string;
 }
 
-const props = withDefaults(defineProps<Props>(), {  dot: `digraph {a -> b}` })
-const graphDivRef = ref<HTMLDivElement>()
+const props = withDefaults(defineProps<Props>(), { dot: `digraph {a -> b}` });
+const graphDivRef = ref<HTMLDivElement>();
 
-let graph: Graphviz<any, any, any, any>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let graph: Graphviz<BaseType, any, BaseType, any>;
 
 const initGraphViz = () => {
-  if (!graphDivRef.value) return
-  graph = graphviz(graphDivRef.value, { useWorker: false })
-  render(props.dot)
-}
-
+  if (!graphDivRef.value) return;
+  graph = graphviz(graphDivRef.value, { useWorker: false });
+  render(props.dot);
+};
 
 function transitionFactory() {
   // We need to create a new transition for each render
-  return d3.transition("main")
-    .duration(750)
-    .ease(d3.easeLinear);
+  return d3.transition("main").duration(750).ease(d3.easeLinear);
 }
 
 const render = (content: string) => {
-  graph.dot(content)
+  graph
+    .dot(content)
     .transition(transitionFactory)
     .zoom(false)
-      .render()
-      .onerror((em) => console.log('graph error', em))
-}
+    .render()
+    .onerror((em) => {
+      console.log("graph error", em);
+    });
+};
 
-watch(() => props.dot, (current, before) => render(current))
-onMounted(async () => initGraphViz());
+watch(
+  () => props.dot,
+  (current, _before) => {
+    render(current);
+  },
+);
+onMounted(() => {
+  initGraphViz();
+});
 </script>
 
 <template>
-    <div ref="graphDivRef" class="graph" />
+  <div
+    ref="graphDivRef"
+    class="graph"
+  />
 </template>
 
 <style>
