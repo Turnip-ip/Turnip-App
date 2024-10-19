66c6f4537345e895b74c9e3a3a1434404a4ed589
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-19 11:22:43 +0200

    Create a ClientOnly component for the turing graph
    to be able to await the wasm module init in the browser

---
+++ components/TuringGraphView.client.vue
@@ -0,0 +1,19 @@
+<template>
+  <GraphView :dot="dot" />
+</template>
+
+<script setup lang="ts">
+import init, { tm_string_to_dot } from "tm_parser?init";
+
+interface Props {
+  code: string;
+}
+
+const props = withDefaults(defineProps<Props>(), { code: `` });
+
+await init();
+
+const dot = computed(() => {
+  return tm_string_to_dot(props.code, "Name");
+});
+</script>
