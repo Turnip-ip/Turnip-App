851201ff8b90bd9b5f2115f14a518e22dfdaf7d7
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-19 11:24:25 +0200

    Format

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <ResizablePanelGroup direction="horizontal">
     <ResizablePanel>
-      <TextEditor v-model="dotArea" class="m-4" height="500px" />
+      <TextEditor
+        v-model="dotArea"
+        class="m-4"
+        height="500px"
+      />
     </ResizablePanel>
     <ResizableHandle with-handle />
     <ResizablePanel>
-      <GraphView class="m-4 w-72 h-82" :dot="dot" />
+      <GraphView
+        class="h-82 m-4 w-72"
+        :dot="dot"
+      />
     </ResizablePanel>
   </ResizablePanelGroup>
 </template>
@@ -16,13 +23,15 @@ import init, { tm_string_to_dot } from "tm_parse?init";
 
 onMounted(() => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-  init().then(() => {
-    console.log("Init finished")
-    inited.value = true
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  }).catch((e: unknown) => {
-    console.error("Init failed", e)
-  });
+  init()
+    .then(() => {
+      console.log("Init finished");
+      inited.value = true;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    })
+    .catch((e: unknown) => {
+      console.error("Init failed", e);
+    });
 });
 
 const inited = ref(false);
@@ -34,15 +43,15 @@ const dotArea = ref<string>(`START
 q
 | 1 -> (0,L), q
 | 0 -> (1,L), END
-`)
+`);
 const dot = computed(() => {
   //await init();
   //console.log("Computing", inited.value, dotArea.value)
   if (inited.value) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
-    return tm_string_to_dot(dotArea.value, 'TEST');
+    return tm_string_to_dot(dotArea.value, "TEST");
   } else {
-    return ""
+    return "";
   }
 });
 </script>
