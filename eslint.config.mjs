669de4da7a0e8435a6b6af93d8c889eb524e5c3e
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-19 10:34:04 +0200

    Enable eslintConfigPrettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintPluginVue from "eslint-plugin-vue";
 import tseslint from "typescript-eslint";
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default withNuxt(
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
@@ -30,4 +31,6 @@ export default withNuxt(
     files: ["layouts/*.vue", "pages/**/*.vue"],
     rules: { "vue/multi-word-component-names": "off" },
   },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  eslintConfigPrettier,
 );
