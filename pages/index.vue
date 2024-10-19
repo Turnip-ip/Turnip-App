bda926069f8208381a338791774c58e627141fdc
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-19 10:38:21 +0200

    Lint and fix errors in Index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,31 @@
 <template>
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel>
-        <TextEditor v-model="dotArea" class="m-4" height="500px"/>
-      </ResizablePanel>
-      <ResizableHandle with-handle />
-      <ResizablePanel>
-        <GraphView class="m-4 w-72 h-82" :dot="dot"/>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+  <ResizablePanelGroup direction="horizontal">
+    <ResizablePanel>
+      <TextEditor v-model="dotArea" class="m-4" height="500px" />
+    </ResizablePanel>
+    <ResizableHandle with-handle />
+    <ResizablePanel>
+      <GraphView class="m-4 w-72 h-82" :dot="dot" />
+    </ResizablePanel>
+  </ResizablePanelGroup>
 </template>
 
 <script setup lang="ts">
-import init, { tm_string_to_dot } from "tm_parser/tm_parser.js?init";
+// @ts-expect-error TODO create a type schime
+import init, { tm_string_to_dot } from "tm_parse?init";
 
 onMounted(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   init().then(() => {
     console.log("Init finished")
     inited.value = true
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  }).catch((e: unknown) => {
+    console.error("Init failed", e)
   });
 });
 
-let inited = ref(false);
+const inited = ref(false);
 
 const dotArea = ref<string>(`START
 | b -> (b,R), START
@@ -34,9 +39,8 @@ const dot = computed(() => {
   //await init();
   //console.log("Computing", inited.value, dotArea.value)
   if (inited.value) {
-    const a = tm_string_to_dot(dotArea.value, 'TEST');
-    console.log(a);
-    return a
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
+    return tm_string_to_dot(dotArea.value, 'TEST');
   } else {
     return ""
   }
