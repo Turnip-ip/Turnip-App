ae9fe3eb76de7b96a29108339b8f3e2d76ab4f3f
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-11-17 12:01:35 +0100

    NuxtLink on level number

---
+++ components/Level.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLink to="/levels/0"><b>Level 0</b></NuxtLink>
+    <p>Description for Level 0</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  cursor: pointer;
+}
+</style>
--- pages/levels/index.vue
+++ pages/levels/index.vue
@@ -1,22 +1,3 @@
 <template>
-    <Level @show-two-tabs="showTabs" />
+    <Level />
 </template>
-  
-
-<script setup>
-import Level from './components/ui/levels/Level.vue';
-const emit = defineEmits(['showTabs']);
-
-
-const showTabs = () => {
-    //show other two tabs if not visible
-    emit('showTabs');
-    //emit('showbuttons');
-    
-    //set current page to Questions page
-    // show the corresponding question
-};
-
-</script>
-
-
