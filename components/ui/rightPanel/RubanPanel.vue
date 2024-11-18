68cf6eaba6c0826a43950599ae65d63cf62dbc7a
Ines BLANCHE <ines.blanche@ens-lyon.fr>
2024-11-18 23:26:23 +0100

    output et ruban pour pres

--- components/ui/rightPanel/RubanPanel.vue
+++ components/ui/rightPanel/RubanPanel.vue
@@ -2,7 +2,9 @@
 <template>
   <div :style="{ backgroundColor: bgColor, padding: '16px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }">
     <slot>
-      <h2>Tape Space</h2>
+      <div class="bubbleImg"><NuxtImg src="bubble_speech.png"></NuxtImg></div>
+      <div class="TapeComingSoon">tape coming soon !</div>
+      <div class="imgTurnip"><NuxtImg src="Turnip_Boy_Portrait_sans_background.jpeg"></NuxtImg></div>
     </slot>
   </div>
 
@@ -17,4 +19,23 @@ const props = defineProps({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.TapeComingSoon{
+  position: absolute;
+  font-family: "Press Start 2P", sans-serif;
+  right: 560px;
+  top:446px
+}
+.imgTurnip{
+    position: absolute;
+    width: 80px;
+    top: 440px;
+    right: 850px;
+  }
+  .bubbleImg{
+    position: absolute;
+    width: 300px;
+    top: 39.5%;
+    right: 30%;
+  }
+</style>
