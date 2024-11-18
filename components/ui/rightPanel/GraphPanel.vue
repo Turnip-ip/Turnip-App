6c6bd329bd87dfe8706ab15f87348ed078d16e9d
Ines BLANCHE <ines.blanche@ens-lyon.fr>
2024-11-18 23:39:37 +0100

    temp pour le graphe

--- components/ui/rightPanel/GraphPanel.vue
+++ components/ui/rightPanel/GraphPanel.vue
@@ -1,9 +1,17 @@
 <!-- components/ui/resizable/RightHalfPanel.vue -->
 <!-- n'est pas ajouté au index.vue car bug quand on rajoute le graphview dedans -->
 <template>
-  <div :style="{ backgroundColor: bgColor, padding: '16px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }">
+  <div
+    :style="{ backgroundColor: bgColor, padding: '30px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', height: '340px' }">
     <slot>
-      <h2> Graph Space</h2>
+      <div class="bubbleImg">
+        <NuxtImg src="bubble_speech.png"></NuxtImg>
+      </div>
+      <div class="TapeComingSoon">Graph here !</div>
+      <div class="imgTurnip">
+        <NuxtImg src="Turnip_Boy_Portrait_sans_background.jpeg"></NuxtImg>
+      </div>
+
       <TuringGraphView class="p-4 h-50" :code="dotArea" />
     </slot>
   </div>
@@ -22,4 +30,25 @@ const props = defineProps({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.TapeComingSoon {
+  position: absolute;
+  font-family: "Press Start 2P", sans-serif;
+  right: 676px;
+  top: 87px
+}
+
+.imgTurnip {
+  position: absolute;
+  width: 70px;
+  top: 80px;
+  right: 868px;
+}
+
+.bubbleImg {
+  position: absolute;
+  width: 210px;
+  top: 7%;
+  right: 36%;
+}
+</style>
