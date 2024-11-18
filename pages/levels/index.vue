c1b38924c8af1d27c26b0330299ba5fea2e269a8
Ines BLANCHE <ines.blanche@ens-lyon.fr>
2024-11-18 16:51:56 +0100

    homepage done encore

--- pages/levels/index.vue
+++ pages/levels/index.vue
@@ -1,9 +1,5 @@
 <template>
-  <GraphView
-    :dot="dot"
-    class="h-82 m-4 w-72"
-    @click="handleBodyClick"
-  />
+  <GraphView :dot="dot" class="h-82 m-4 w-72" @click="handleBodyClick" />
 </template>
 
 <script setup lang="ts">
