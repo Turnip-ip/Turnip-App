520980b8234711836104c4ea4bc3ebf22699803a
Ines BLANCHE <ines.blanche@ens-lyon.fr>
2025-01-21 10:34:39 +0100

    added text for question

--- pages/levels/[id]/index.vue
+++ pages/levels/[id]/index.vue
@@ -2,39 +2,51 @@
   <div>
     <!-- Centered text content -->
     <div class="flex h-screen items-center justify-center">
-      <div class="imageBubble"> <NuxtImg src="bubble_speech.png"></NuxtImg></div>
-      <div class="imageTurnip"><NuxtImg src="Turnip_Boy_Portrait_sans_background.jpeg"></NuxtImg></div>
+      <div class="imageBubble">
+        <NuxtImg src="bubble_speech.png"></NuxtImg>
+      </div>
+      <div class="imageTurnip">
+        <NuxtImg src="Turnip_Boy_Portrait_sans_background.jpeg"></NuxtImg>
+      </div>
       <!-- pour la demo, a modifier quand on fera l'integration de toutes les questions-->
-      <div class="questionText">Given the current position on the tape, write a program to move the cursor to the right.</div>
-    <div class="submitText">
+      <div v-if="questionText" class="questionText">{{ questionText }}</div>
+      <!--<div class="questionText">Given the current position on the tape, write a program to move the cursor to the right.</div>-->
+      <div class="submitText">
         Click on
-        <NuxtLink
-          to="/levels/id/Answer"
-          class="answer"
-          >Answer</NuxtLink
-        >
-        to submit your code !</div>
+        <NuxtLink to="/levels/id/Answer" class="answer">Answer</NuxtLink>
+        to submit your code !
+      </div>
     </div>
   </div>
 </template>
 
+
 <script setup lang="ts">
 import { onMounted } from 'vue';
 
-let levels_data={};
+let questionText = "";
+
+let levels_data = {};
 onMounted(async () => {
   try {
-    const response = await fetch('../../levels_contents.json')
+    console.log("step1");
+    const response = await fetch('../../levels_contents.json');
+    console.log("step3");
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    levels_data = await response.json();
-    //call create graph
-    console.log(levels_data);
+    const levels_data = await response.json();
+    console.log("step2");
+    const current_level = localStorage.getItem("current_level");
+    console.log("retrieved current level ");
+    questionText = levels_data["levels"][current_level]["description"];
   } catch (error) {
-    console.error('Error fetching JSON:', error)
+    console.error("Failed to load question text:", error);
+    questionText = "Error loading question.";
   }
-});
+}
+);
+
 
 
 
@@ -52,19 +64,22 @@ definePageMeta({
 .h-screen {
   height: 100vh;
 }
-.imageBubble{
+
+.imageBubble {
   position: absolute;
   bottom: 40%;
   height: 40%;
   width: 80%;
 }
-.imageTurnip{
+
+.imageTurnip {
   position: absolute;
   height: 200px;
   width: 400px;
   left: 10%;
 }
-.questionText{
+
+.questionText {
   position: absolute;
   bottom: 61%;
   font-family: "Press Start 2P", sans-serif;
@@ -74,6 +89,7 @@ definePageMeta({
   width: 1500px;
 
 }
+
 .submitText {
   position: absolute;
   font-family: "Press Start 2P", sans-serif;
@@ -82,8 +98,8 @@ definePageMeta({
   left: 700px;
   bottom: 350px;
 }
+
 .answer {
   color: darkslateblue;
 }
-
 </style>
