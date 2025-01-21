69b212ad456f6690467c4f549bc72d16de5ec786
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-01-21 11:29:27 +0100

    Pass level id as path param and use navigateTo

---
+++ pages/levels/texts/[id].vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex h-screen items-center justify-center">
+    <div id="content"> {{ content }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+import { LevelsData } from '~/lib/levels_data';
+
+const currentTextId: string = route.params.id as string;
+
+const content = LevelsData.texts[currentTextId].content;
+
+// add to completed
+add_completed_lvl(currentTextId);
+
+function add_completed_lvl(textId: string) {
+  // read completed_lvl in localStorage
+  let arr_compl;
+  let res = localStorage.getItem("completed_lvl");
+  if (res == null) arr_compl = [];
+  else arr_compl = JSON.parse(res);
+  // add and save
+  if (!arr_compl.includes(textId)) arr_compl.push(textId);
+  localStorage.setItem("completed_lvl", JSON.stringify(arr_compl));
+}
+</script>
+
+<style scoped>
+/* Ensures the text is centered vertically and horizontally */
+.h-screen {
+  height: 100vh;
+}
+
+#content {
+  position: absolute;
+  margin: auto 300px;
+  font-family: "Press Start 2P", sans-serif;
+  color: darkslateblue;
+  font-size: 20px;
+  text-align: center;
+}
+</style>
