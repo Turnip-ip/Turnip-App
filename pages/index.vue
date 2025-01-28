ebc6a63568ebf6ed0da55d61b3c38f56a061ae22
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-01-28 10:25:16 +0100

    Responsive first page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="flex h-screen flex-col items-center md:flex-row">
-    <div class="w-1/2">
+  <div class="flex h-screen flex-col-reverse items-center md:flex-row">
+    <div class="md:w-1/2">
       <NuxtImg src="Turnip_Boy_Portrait_sans_background.jpeg" class="w-max" />
     </div>
-    <!-- <h1 class="imag w-1/2">
-      <NuxtImg src="/Turnip_Boy_Portrait_sans_background.jpeg" class="framed-imag" />
-    </h1> -->
-    <div class="flex w-1/2 flex-col items-center justify-center gap-2">
-      <TurnipText />
+
+    <div class="flex md:w-1/2 flex-col items-center justify-center gap-2">
+      <TurnipText class="w-5/6" />
       <h1 class="title text-center text-2xl m-2">
         <NuxtLink to="/levels" class="niveau h-20 w-100  p-2 rounded-sm hover:bg-[#ffe9f4]" variant="outline">Play
         </NuxtLink>
