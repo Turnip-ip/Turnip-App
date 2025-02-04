bc0d8ce2557df71f8e5238ddb192e9706d2abb99
inesbl <118980535+inesbl@users.noreply.github.com>
2025-02-04 10:52:35 +0100

    Interface (#9)
    * homepage done encore
    
    * question done
    
    * output et ruban pour pres
    
    * Change bubble
    
    * Format
    
    ---------
    
    Co-authored-by: Ines BLANCHE <ines.blanche@ens-lyon.fr>
    Co-authored-by: armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>

--- components/ui/rightPanel/OutputPanel.vue
+++ components/ui/rightPanel/OutputPanel.vue
@@ -1,43 +1,49 @@
 <!-- components/ui/resizable/RightHalfPanel.vue -->
 <template>
-    <div :style="{ backgroundColor: bgColor, padding: '16px', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', height: '400px' }">
-      <slot>
-        <div class="bubbleImg"><NuxtImg src="bubble_speech.png"></NuxtImg></div>
+  <div
+    :style="{ backgroundColor: bgColor, padding: '16px', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', height: '400px' }">
+    <slot>
+      <div class="bubbleImg">
+        <NuxtImg src="bubble_speech.png"></NuxtImg>
+      </div>
       <div class="TapeComingSoon">output coming soon !</div>
-      <div class="imgTurnip"><NuxtImg src="Turnip_Boy_Portrait_sans_background.jpeg"></NuxtImg></div>
-   
-      </slot>
-    </div>
-    
-  </template>
-  
-  <script setup lang="ts">
-  const props = defineProps({
-    bgColor: {
-      type: String,
-      default: ' #D9DFE5'   
-    }
-  })
-  </script>
-  
-  <style scoped>
-.TapeComingSoon{
+      <div class="imgTurnip">
+        <NuxtImg src="Turnip_Boy_Portrait_sans_background.jpeg"></NuxtImg>
+      </div>
+
+    </slot>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  bgColor: {
+    type: String,
+    default: ' #D9DFE5'
+  }
+})
+</script>
+
+<style scoped>
+.TapeComingSoon {
   position: absolute;
   font-family: "Press Start 2P", sans-serif;
   right: 507px;
-  top:773px
+  top: 773px
 }
-.imgTurnip{
-    position: absolute;
-    width: 100px;
-    top: 760px;
-    right: 830px;
-  }
-  .bubbleImg{
-    position: absolute;
-    width: 340px;
-    top: 750px;
-    right: 500px;
-  }
-  </style>
-  
+
+.imgTurnip {
+  position: absolute;
+  width: 100px;
+  top: 760px;
+  right: 830px;
+}
+
+.bubbleImg {
+  position: absolute;
+  width: 340px;
+  top: 750px;
+  right: 500px;
+}
+</style>
