b8da48ed65a95817a4b50acedd286f856800290d
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-02-17 16:18:59 +0100

    Run machine

---
+++ components/TuringGraphView.vue
@@ -0,0 +1,11 @@
+<template>
+  <GraphView :dot="dot" />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  dot: string;
+}
+
+const _props = withDefaults(defineProps<Props>(), { dot: '' });
+</script>
